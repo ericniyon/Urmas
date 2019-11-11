1a7403acd3b8c1a348700f9ef0f0fb7ce6f17af0
madiba <niyoeri6@gmail.com>
2019-11-11 19:43:01 +0200

    complete stage project

---
+++ dashboad/api/serializers.py
@@ -0,0 +1,15 @@
+from dashboad.models import (
+    Staff, 
+    Mission, 
+    SchoolModel, 
+    SchoolSupervisor,
+    Category,
+    Department,
+    TransportMean,
+    Report,
+    Role
+)
+from rest_framework import serializers
+
+
+
